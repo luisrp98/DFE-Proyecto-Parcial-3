93f9989dd5de52230cd08bed2a29075e2ed08c7c
Luis Romo <72539524+luisrp98@users.noreply.github.com>
2023-11-26 17:21:36 -0700

    add default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
 import { ReportsComponent } from './reports/reports/reports.component';
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'reportes', component: ReportsComponent },
 ];
