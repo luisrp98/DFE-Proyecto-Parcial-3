04e58d65b1b9b2ee44f9e3c7a275709eae3d6df9
Luis Romo <72539524+luisrp98@users.noreply.github.com>
2023-11-28 18:08:31 -0700

    add componets for reports

--- src/app/reports/reports.module.ts
+++ src/app/reports/reports.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReportsComponent } from './reports/reports.component';
+import { SalesComponent } from './sales/sales.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
-  declarations: [ReportsComponent],
+  declarations: [ReportsComponent, SalesComponent, DetailsComponent],
   imports: [CommonModule],
   exports: [ReportsComponent],
 })
---
+++ src/app/reports/sales/sales.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales',
+  templateUrl: './sales.component.html',
+  styles: [
+  ]
+})
+export class SalesComponent {
+
+}
